0d27145258849a28552fb23d29eb295af3f1c983
Covenant Junior <covenant@gmail.com>
2023-05-24 22:43:32 +0100

    Math Import and Constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color.fromRGBO(51, 153, 255, 1.0);
 // const TargetPlatform platform = TargetPlatform.android;
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FibonacciSequence());
+}
+
+class FibonacciSequence {
+  static const seedRadius = 2.0; // Seeds
+  static const scaleFactor = 4;  // Scale
+  static const tau = math.pi * 2;  // Tau
 }
