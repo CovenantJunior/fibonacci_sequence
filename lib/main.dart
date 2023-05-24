6dc149c967a1da31cb04ba2040b309436eb239e9
Covenant Junior <covenant@gmail.com>
2023-05-24 22:34:52 +0100

    Set theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const Color primaryColor = Color.fromRGBO(51, 153, 255, 1.0);
+// const TargetPlatform platform = TargetPlatform.android;
+
 void main() {
   runApp(const MyApp());
 }
