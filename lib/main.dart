b1460d7cfb1a2a940f3f420eb602ed96fd0fde00
Covenant Junior <covenant@gmail.com>
2023-05-24 23:19:23 +0100

    Not a const constructor

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ const Color primaryColor = Color.fromRGBO(51, 153, 255, 1.0);
 // const TargetPlatform platform = TargetPlatform.android;
 
 void main() {
-  runApp(const FibonacciSequence());
+  runApp(FibonacciSequence());
 }
 
 class FibonacciSequencePainter {}
